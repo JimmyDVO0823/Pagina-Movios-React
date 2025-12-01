c238ce65191fef878691fa3da5409028eb6bbde1
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-12-01 12:30:17 -0500

    Creamos el primer componente, ahora react va tomando sentido

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import React from "react";
-import ReactDom from "react-dom/client";
+import React from "react"
+import ReactDom from "react-dom/client"
 
-const element = document.getElementById("root");
+const root = ReactDom.createRoot(document.getElementById("root")); //Creamos el elemento raiz
+//Los componentes en react son funciones que retornan etiquetas
+function Hello() {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+    </div>
+  );
+}
+
+//Renderizamos la raiz
+root.render(
+  <div>
+    <Hello />
+  </div>
+);

6759a9ab19313d0521581b6c4614a6f3755a59fb
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-12-14 13:52:35 -0500

    Aprendimos a utilizar los arreglos (un poco)

--- src/index.js
+++ src/index.js
@@ -2,17 +2,46 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { Button } from "./Button";
 import { Card } from "./Task";
-import {Post} from "./Posts"
+import { Post } from "./Posts";
 
 const root = ReactDom.createRoot(document.getElementById("root")); //Creamos el elemento raiz
 //Los componentes en react son funciones que retornan etiquetas
 const prevent = (e) => {
-  e.preventDefault()
-  alert("hola")
-  console.log("no se elimina")
-}
+  e.preventDefault();
+  alert("hola");
+  console.log("no se elimina");
+};
 
-let buttonText = "Fetch Data"
+let buttonText = "Fetch Data";
+
+const people = [
+  {
+    id: 1,
+    name: "D'Franco",
+    url: "https://robohash.org/person1",
+  },
+  {
+    id: 2,
+    name: "Heidy",
+    url: "https://robohash.org/person2",
+  },
+  {
+    id: 3,
+    name: "Max",
+    url: "https://robohash.org/person3",
+  },
+];
+
+function renderUser() {
+  return people.map((user, i) => {
+    return (
+      <div>
+        <h1>{user.id + ") " + user.name}</h1>
+        <img src={user.url} />
+      </div>
+    );
+  });
+}
 
 //Renderizamos la raiz
 root.render(
@@ -26,11 +55,10 @@ root.render(
       }}
     />
     <form onSubmit={prevent}>
-      <h1>
-        Formulario
-      </h1>
+      <h1>Formulario</h1>
       <button>Button</button>
     </form>
-    <Post text = {buttonText}/>
+    <Post text={buttonText} />
+    {renderUser()}
   </>
 );

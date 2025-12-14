5efbca76420f718dded963b1875766e431d40c2e
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-12-14 11:43:08 -0500

    Ahora se aprendió a manejar librerias externas. React-Icons para probar. Hay que buscar librerias para crear menus de musica y eso

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,9 +1,16 @@
+import { RiMusicFill } from "react-icons/ri";
+
 export const Post = ({ text }) => {
-  return <button onClick={fetchData}>{text}</button>;
+  return (
+    <button onClick={fetchData}>
+      <RiMusicFill />
+      {text}
+    </button>
+  );
 };
 
 function fetchData() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
+  fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log(error));
